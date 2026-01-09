c62fb9166d38e1f5389b1f2c30e6ad40bbd74fb4
Anthony Erreyes <antony23w@gmail.com>
2026-01-08 22:21:25 -0500

    Fix: Force config cache clear on startup

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Clearing caches..."
+php artisan optimize:clear
+
+echo "Caching configuration..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
 
