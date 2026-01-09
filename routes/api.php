4b42ac69c3aa736f3d2b56f7109520b5a0c37233
Anthony Erreyes <antony23w@gmail.com>
2026-01-08 23:59:27 -0500

    fix: Add login API endpoint and change login background to white

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,11 @@ Route::get('/user', function (Request $request) {
 
 // Public routes for now (or protected if we implement auth later)
 Route::prefix('v1')->group(function () {
+    // Auth routes
+    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+    
+    // Product routes
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
